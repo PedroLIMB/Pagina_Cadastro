de609caea9cc4b76a142384c4db21bee08031dc4
PedroLIMB <githublimb@gmail.com>
2023-08-21 15:26:22 -0300

    V1.5

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function validarSenha(){
         alert('Concluimos seu cadastro!')
     }
 }
+
+function loginSenha(){
+    var senha = 'loginbolo'
+    var senha2 = document.getElementById('lsenha').value
+
+    if(senha2 != senha){
+        alert('Senha Incorreta')
+    }else{
+        alert('Bem vindo de volta!')
+    }
+}
